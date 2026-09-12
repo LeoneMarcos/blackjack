7cf6bb23e0649b762a99cc3d9d899f9dc3573db2
Leone Marcos <contact@leonemarcos.com>
2026-09-12 08:19:09 -0300

    chore: remove worker-specific eslint ignore

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['dist', 'node_modules', 'showcase-assets', '**/.wrangler/**'],
+    ignores: ['dist', 'node_modules', 'showcase-assets'],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
